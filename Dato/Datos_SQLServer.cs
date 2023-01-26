3054c69988300aff286b72cce24f065d367baca0
Michael Vargas S <michaelvargas47@hotmail.com>
2023-01-25 19:16:39 -0600

    Revert "Elimino metodos de mas en datos"
    This reverts commit d7be5c711fa5044714235b914bdbcd9e60c6b3a5.

--- Dato/Datos_SQLServer.cs
+++ Dato/Datos_SQLServer.cs
@@ -39,6 +39,162 @@ namespace Datos
             }
         }//fn constructor
 
+        public Boolean probarConexion_SP(string usuarioValidar,string passValidar)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SP_ValidaUsuario", sqlConector) 
+                {
+                CommandType=System.Data.CommandType.StoredProcedure
+                };
+                cmd.Connection.Open();
+                cmd.Parameters.Add("@usuario",SqlDbType.VarChar,50).Value=usuarioValidar;
+                cmd.Parameters.Add("@clave", SqlDbType.VarChar, 50).Value = passValidar;
+                cmd.Parameters.Add("@patron", SqlDbType.VarChar, 50).Value = "encripta1";
+                 SqlDataReader dr=cmd.ExecuteReader();
+
+                if (dr.Read()) 
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+                
+            }
+            finally { this.sqlConector.Close(); }   
+        }//fn prueba conec
+        public Boolean probarConexionSQLServer()
+        {
+            try
+            {
+                //solo abrimos y cerramos la conexion para saber que el usuario viene bien 
+                sqlConector.Open();
+
+                return true;
+            }
+            catch (Exception Error)
+            {
+                throw new Exception(Error.Message);
+            }
+            finally
+            {
+                sqlConector.Close();
+            }
+
+        }//fin querySQL_Server
+        public Boolean probarConexionSQLServer_Win_Autentifi()
+        {
+            try
+            {
+                this.sqlConector = new SqlConnection("Data Source=" + ConfigurationManager.AppSettings["DataSource"] + ";Initial Catalog=master;Integrated Security=True");
+
+                //solo abrimos y cerramos la conexion para saber que el usuario viene bien 
+                sqlConector.Open();
+
+                return true;
+            }
+            catch (Exception Error)
+            {
+                throw new Exception(Error.Message);
+            }
+            finally
+            {
+                sqlConector.Close();
+            }
+
+        }//fin querySQL_Server
+        public DataTable QuerySQL_Server(String QueryDelUsuario)
+        {
+            try
+            {
+                SqlCommand cmd = sqlConector.CreateCommand();
+                cmd.CommandText = QueryDelUsuario;
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                DataTable dtdatos = new DataTable();
+
+                sqlConector.Open();
+
+                adapter.Fill(dtdatos);
+
+                return dtdatos;
+            }
+            catch (Exception Error)
+            {
+                throw new Exception(Error.Message);
+            }
+            finally
+            {
+                sqlConector.Close();
+            }
+
+        }//fin querySQL_Server
+        public DataTable QuerySQL_Server_Win_Autentifi(String QueryDelUsuario)
+        {
+            try
+            {
+                this.sqlConector = new SqlConnection("Data Source=" + ConfigurationManager.AppSettings["DataSource"] + ";Initial Catalog=master;Integrated Security=True");
+
+                SqlCommand cmd = sqlConector.CreateCommand();
+                cmd.CommandText = QueryDelUsuario;
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                DataTable dtdatos = new DataTable();
+
+                sqlConector.Open();
+
+                adapter.Fill(dtdatos);
+
+                return dtdatos;
+            }
+            catch (Exception Error)
+            {
+                throw new Exception(Error.Message);
+            }
+            finally
+            {
+                sqlConector.Close();
+            }
+
+        }//fin querySQL_Server
+
+
+        //public Boolean InsertarUsuario(string usuario, string password)
+        //{
+        //    try
+        //    {
+        //        SqlCommand cmd = new SqlCommand("SP_insertar", sqlConector)
+        //        {
+        //            CommandType = System.Data.CommandType.StoredProcedure
+        //        };
+        //        cmd.Connection.Open();
+        //        cmd.Parameters.Add("@usuario", SqlDbType.VarChar, 20).Value = usuario;
+        //        cmd.Parameters.Add("@clave", SqlDbType.VarChar, 20).Value = password;
+        //        //cmd.Parameters.Add("@patron", SqlDbType.VarChar, 50).Value = "encripta1";
+        //       cmd.ExecuteNonQuery();
+
+               
+        //            return true;
+              
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        throw new Exception(ex.Message);
+
+        //    }
+        //    finally { this.sqlConector.Close(); }
+        //}//fn prueba conec
+
+
+
         public DataTable login(Usuario objUsuario)
         {
             try
